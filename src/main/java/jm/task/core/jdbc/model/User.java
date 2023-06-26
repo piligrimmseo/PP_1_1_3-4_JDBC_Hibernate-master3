35728c1f08c894d5bdb8ad4badcbc6c88f3252ca
Dmitrii <piligrimm.seo@gmail.com>
2023-06-26 12:00:20 +0300

    hibernate

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -10,13 +10,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column (name = "name")
     private String name;
 
     @Column (name = "last_name")
     private String lastName;
 
-    @Column
+    @Column (name = "age")
     private Byte age;
 
     public User() {
